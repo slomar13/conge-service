5d0179169a6229b617263fe785a255d99efd6650
Omar Slaoui <slaouiomar50@gmail.com>
2025-11-08 20:37:16 +0100

    Initial commit

---
+++ src/main/java/com/emsi/conge_service/CongeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.emsi.conge_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CongeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsi/conge_service/CongeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.conge_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
