a294b1ce22689d86d5f21d40d0e949100a3acf6e
Hamza Slaoui <slaouihza2@gmail.com>
2025-11-22 16:21:49 +0100

    ajout de l'annotation @FeignClient

--- src/main/java/com/emsi/conge_service/CongeServiceApplication.java
+++ src/main/java/com/emsi/conge_service/CongeServiceApplication.java
@@ -3,10 +3,12 @@ package com.emsi.conge_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CongeServiceApplication {
 
 	public static void main(String[] args) {
