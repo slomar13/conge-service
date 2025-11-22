046c0e11bf68ba716ee560c2d6850415b31655b8
Hamza Slaoui <slaouihza2@gmail.com>
2025-11-22 16:29:16 +0100

    ajout de l'exception DateConflictException

---
+++ src/main/java/com/emsi/conge_service/exception/DateConflictException.java
@@ -0,0 +1,7 @@
+package com.emsi.conge_service.exception;
+
+public class DateConflictException extends RuntimeException{
+    public DateConflictException(String message){
+        super(message);
+    }
+}
