a390c09e99b7c5c84a5836552f8d79a7d7a8abaa
Hamza Slaoui <slaouihza2@gmail.com>
2025-11-22 16:27:17 +0100

    correction de la methode existsByEmployeIdAndDatesOverlap

--- src/main/java/com/emsi/conge_service/repository/CongeRepository.java
+++ src/main/java/com/emsi/conge_service/repository/CongeRepository.java
@@ -3,6 +3,8 @@ package com.emsi.conge_service.repository;
 import com.emsi.conge_service.entity.Conge;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,5 +12,12 @@ import java.util.List;
 public interface CongeRepository extends JpaRepository<Conge, Long> {
     List<Conge> findByEmployeId(Long employeId);
 
-    boolean existsByEmployeIdAndDatesOverlap(Long employeId,  LocalDate dateDebut,  LocalDate dateFin);
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Conge c " +
+            "WHERE c.employeId = :employeId " +
+            "AND (c.dateDebut <= :dateFin AND c.dateFin >= :dateDebut)")
+    boolean existsByEmployeIdAndDatesOverlap(
+            @Param("employeId") Long employeId,
+            @Param("dateDebut") LocalDate dateDebut,
+            @Param("dateFin") LocalDate dateFin
+    );
 }
